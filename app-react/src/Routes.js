c088f63c24ad7eeebcdd077e8c1a005fa0e47d1d
Julius Williams <jmichaelcodes@gmail.com>
2017-04-04 13:13:14 -0400

    sign in page done

--- app-react/src/Routes.js
+++ app-react/src/Routes.js
@@ -8,12 +8,16 @@ import { Router, Route, browserHistory } from 'react-router'
 // Import your Todos and Completed components here...
 
 import Main from './Main'
+import Signin from './Signin'
+import Signup from './Signup'
 
 // Configure routes
 class Routes extends React.Component {
     render() {
         return <Router history={browserHistory}>
             <Route path="/" component={Main} />
+            <Route path="/signin" component={Signin} />
+            <Route path="/signup" component={Signup} />
         </Router>
     }
 }
