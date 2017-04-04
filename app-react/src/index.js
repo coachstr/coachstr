d70f9815df8388e996abd42c1f3efe5032faa425
Julius Williams <jmichaelcodes@gmail.com>
2017-04-04 10:21:18 -0400

    react set up

---
+++ app-react/src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+// import { Router, Route, browserHistory, IndexRoute } from 'react-router'
+
+import Routes from './Routes'
+
+ReactDOM.render(
+  <Routes />,
+  document.getElementById('root')
+);
