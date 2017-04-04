1bf11a5d4825e62e5e3962008bf87e195f341572
Michael Stashevsky <mstashev@gmail.com>
2017-04-03 20:22:52 -0400

    fixed migration

--- db/migrate/20170403234845_create_taggings.rb
+++ db/migrate/20170403234845_create_taggings.rb
@@ -1,8 +1,10 @@
 class CreateTaggings < ActiveRecord::Migration[5.0]
   def change
     create_table :taggings do |t|
-      t.references :tag_id, foreign_key: true
-      t.belongs_to :taggable, polymorphic: true
+      t.references :tag, foreign_key: true
+      t.integer :taggable_id
+      t.string :taggable_type
+      # t.belongs_to :taggable, polymorphic: true
 
       t.timestamps
     end
