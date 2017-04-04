ece5373315c0dd027d00f98b0ead90c6eb06ea78
Michael Stashevsky <mstashev@gmail.com>
2017-04-04 15:14:42 -0400

    updated UsersController

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
 
   def create
-  @user = User.new(user_params)
+    @user = User.new(user_params)
     if @user.save
       UserMailer.signup(@user).deliver
       session[:user_id] = @user.id
@@ -18,4 +18,9 @@ class UsersController < ApplicationController
     params.permit(:name, :email, :password)
   end
 
+<<<<<<< HEAD
+=======
+
+
+>>>>>>> updated UsersController
 end
