cb435be172976d721fa78178445ca0d626a745b2
Sierra <sierramroberts.96@gmail.com>
2017-04-05 09:53:31 -0400

    signup done

--- app-react/src/Signup.js
+++ app-react/src/Signup.js
@@ -18,7 +18,53 @@ class Signup extends Component {
 
     render() {
         return (
-            <div>Sign Up</div>
+            <div>
+            <div className="jumbotron text-center">
+                <h1>Coachstr</h1> 
+                <p>We're Talking About Practice</p>
+            </div>
+            <div className="col s6 m6 container signinCard text-center">
+                {/*<h2 className="header signinCard">Sign In</h2>*/}
+                <div className="card horizontal">
+                    <div className="card-image">
+                        <img src="https://unsplash.it/200" id="signinImage" />
+                    </div>
+                    <div className="card-stacked">
+                        <div className="card-content">
+                            <form action="#">
+                                <div className="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
+                                    <input className="mdl-textfield__input" type="text" id="name" />
+                                    <label className="mdl-textfield__label" htmlFor="name">Name</label>
+                                </div>
+                            </form>
+                            <form action="#">
+                                <div className="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
+                                    <input className="mdl-textfield__input" type="text" id="email"  value={this.state.email} onChange={(e) => this.setState({email: e.target.value})}/>
+                                    <label className="mdl-textfield__label" htmlFor="email">Email</label>
+                                </div>
+                            </form>
+                            <form action="#">
+                                <div className="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
+                                    <input className="mdl-textfield__input" type="password" id="password" />
+                                    <label className="mdl-textfield__label" htmlFor="password">Password</label>
+                                </div>
+                            </form>
+                            <form action="#">
+                                <div className="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
+                                    <input className="mdl-textfield__input" type="password" id="confirmPassword" />
+                                    <label className="mdl-textfield__label" htmlFor="confirmPassword">Confirm Password</label>
+                                </div>
+                            </form>
+                        </div>
+                        <div className="card-action">
+                            <div className="waves-effect waves-light btn-large" onClick={() => browserHistory.push('/')}>Sign Up</div>
+                            <br/>
+                            <div className="waves-effect waves-light " onClick={() => browserHistory.push('/signup')}></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            </div>
         )
     }
 }
