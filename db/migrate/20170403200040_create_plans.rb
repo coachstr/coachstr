f0f71c4b0ceb4d3f4e9c6e713257a8210bda5fe1
Michael Stashevsky <mstashev@gmail.com>
2017-04-06 13:39:38 -0400

    Removed Start Times from Plans

--- db/migrate/20170403200040_create_plans.rb
+++ db/migrate/20170403200040_create_plans.rb
@@ -3,7 +3,6 @@ class CreatePlans < ActiveRecord::Migration[5.0]
     create_table :plans do |t|
       t.string :title
       t.integer :total_duration
-      t.timestamp :start_time
 
       t.timestamps
     end
