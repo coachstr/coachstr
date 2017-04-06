ebe40112d607bad485e8aa0c78cc3c491ea5f6dc
Julius Williams <jmichaelcodes@gmail.com>
2017-04-06 13:32:17 -0400

    signin working

--- app-react/src/components/Header.js
+++ app-react/src/components/Header.js
@@ -4,13 +4,17 @@ import { browserHistory } from 'react-router';
 
 class Header extends Component {
 
+  signout() {
+    sessionStorage.clear()
+    browserHistory.push('/')
+  }
 
     render() {
         return (  <nav>
     <div className="nav-wrapper center orange darken-1">
       <a href="#" className="brand-logo center black-text">Coachstr</a>
       <ul id="nav-mobile" className="right hide-on-med-and-down">
-        <li><a className="black-text" onClick={() => browserHistory.push('/')}>Sign Out</a></li>
+        <li><a className="black-text" onClick={this.signout}>Sign Out</a></li>
       </ul>
     </div>
   </nav>
