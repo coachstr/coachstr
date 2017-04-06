f92a03caa6b4167383fd067b87a68cfcfbf62a5b
Ben Call <bgcall@indiana.edu>
2017-04-06 10:33:14 -0400

    added environment

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -52,6 +52,9 @@ Rails.application.configure do
   # Use a different cache store in production.
   # config.cache_store = :mem_cache_store
 
+  # config.action_mailer.default_url_options = { :host => "sock-o-ramma.herokuapp.com" }
+
+
   # Use a real queuing backend for Active Job (and separate queues per environment)
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "Coachstr_#{Rails.env}"
