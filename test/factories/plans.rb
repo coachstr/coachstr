54d153a4910dd1c865dcc5e0b9bb031c13b20b1d
Michael Stashevsky <mstashev@gmail.com>
2017-04-11 14:16:24 -0400

    updated factories for Plans and Tags.

--- test/factories/plans.rb
+++ test/factories/plans.rb
@@ -1,5 +1,28 @@
-FactoryGirl.define do
+FactoryGirl.define do |plan|
   factory :plan do
-    
+    title           {Faker::Job.title}
+    total_duration  {1500}
+    plan.user       {Factory(:user)}
+
+    transient do
+      drill_count 5
+      tag_count   5
+    end
+
+    factory :plan_with_tags do
+      after(:create) do |job, evaluator|
+        evaluator.tag_count.times do
+          job.tags << build(:tag)
+        end
+      end
+    end
+
+    factory :plan_with_drills do
+      after(:create) do |job, evaluator|
+        evaluator.drill_count.times do
+          job.drills << build(:drills)
+        end
+      end
+    end
   end
 end
--- test/factories/tags.rb
+++ test/factories/tags.rb
@@ -1,5 +1,5 @@
 FactoryGirl.define do
   factory :tag do
-    
+      name {Faker::Job.key_skill}
   end
 end
