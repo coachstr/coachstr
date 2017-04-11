45883d66a3216f83a13f9f8ccd6d301962bee3ea
Michael Stashevsky <mstashev@gmail.com>
2017-04-11 14:18:53 -0400

    Updated Drill factory

--- test/factories/drills.rb
+++ test/factories/drills.rb
@@ -1,5 +1,21 @@
-FactoryGirl.define do
+FactoryGirl.define do |drill|
   factory :drill do
-    
+    title       {Faker::Job.title}
+    description {Faker::Hipster.paragraphs(1)}
+    duration    {300}
+    drill.user  {Factory(:user)}
+
+    transient do
+      tag_count   5
+    end
+
+    factory :drill_with_tags do
+      after(:create) do |job, evaluator|
+        evaluator.tag_count.times do
+          job.tags << build(:tag)
+        end
+      end
+    end
+
   end
 end
