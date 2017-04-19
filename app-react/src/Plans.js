6d44aa4521ae41d8385e8237f2a109243194e8ad
Julius Williams <jmichaelcodes@gmail.com>
2017-04-19 11:10:55 -0400

    fixed new plan button2

--- app-react/src/Plans.js
+++ app-react/src/Plans.js
@@ -58,7 +58,7 @@ class Plans extends Component {
 
         <div className="container">
           <a className="btn-floating btn-large waves-effect waves-light red" onClick={this.addPlan}><i className="material-icons">add</i></a>
-        </div>
+        </div> 
 
         <div className="container">
 
